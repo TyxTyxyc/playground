72ace3f9f3f6a5b57f300168a8adde9e836066d7
Artyom Kondakov <tyxtyxyc@gmail.com>
2015-06-04 14:42:13 +0600

    second bug file for pickle bug

---
+++ sknn_pickle_save_bug_2.py
@@ -0,0 +1,42 @@
+import cPickle as pickle
+
+from sklearn import datasets, cross_validation
+from sknn.mlp import Classifier, Layer, Convolution
+
+digits = datasets.load_digits()
+X = digits.images
+y = digits.target
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+dense_layer_1 = Layer(
+    type='Rectifier',
+    name='Dense_Layer_1',
+    units=128,
+    )
+
+output_layer = Layer(
+    type='Softmax',
+    name='Output_Layer',
+    )
+
+net = Classifier(
+    layers=[dense_layer_1,
+            output_layer],
+    learning_rate=0.003,
+    n_iter=10,
+    n_stable=10,
+    verbose=True,
+    learning_rule='momentum',
+    batch_size=10,
+    valid_size=0.1,
+    weight_decay=0.0005,
+    dropout_rate=0.03
+    )
+
+net.fit(X_train, y_train)
+
+name = 'nnet_f'
+outfile = open(name, 'wb')
+pickle.dump(net, outfile)
+outfile.close()
